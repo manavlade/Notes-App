02c50d5dfe42ff9ca8b3ae49873e94d6e2dfc3ee
manavlade <manavlade14690@gmail.com>
2026-05-16 00:49:47 +0530

    db connection done

--- notes-backend/src/config/postgres.ts
+++ notes-backend/src/config/postgres.ts
@@ -34,9 +34,10 @@
 
 import postgres from "postgres";
 import dotenv from "dotenv";
-import path from "path";
 
-dotenv.config({ path: path.resolve(process.cwd(), '../.env') });
+dotenv.config();
+
+console.log(process.env.DATABASE_URL);
 
 if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL is missing in .env");
